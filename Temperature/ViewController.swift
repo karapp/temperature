3dbf6244dfe25ca46b4650eba521bf667fdd7dd2
Andreas Ropel <andreasropel@Tove.local>
2015-04-27 13:40:15 +0200

    Initial Commit

---
+++ Temperature/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Temperature
+//
+//  Created by Andreas Ropel on 2015-04-27.
+//  Copyright (c) 2015 RopelOfSweden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
